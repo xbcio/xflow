6b16c7f384d261e8feb68733d404667e749ad064
ypli0629 <liyapeng0629@gmail.com>
2026-06-30 15:36:01 +0800

    test(trigger): cover ha/backpressure behavior

---
+++ sdk/xflow/trigger_ha_test.go
@@ -0,0 +1,48 @@
+package xflow
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/xbcio/xflow/backend/memory"
+)
+
+func TestTriggerDedupSharedPrimitivesAllowsOnlyOneInstance(t *testing.T) {
+	primitives := memory.New().TriggerPrimitives()
+	ctx := context.Background()
+
+	first, err := primitives.Dedup(ctx, "event:1", time.Minute)
+	if err != nil {
+		t.Fatal(err)
+	}
+	second, err := primitives.Dedup(ctx, "event:1", time.Minute)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !first || second {
+		t.Fatalf("dedup results = %v/%v, want true/false", first, second)
+	}
+}
+
+func TestCronSingletonSharedPrimitivesAllowsOnlyOneInstance(t *testing.T) {
+	primitives := memory.New().TriggerPrimitives()
+	ctx := context.Background()
+
+	lock, ok, err := primitives.TryLock(ctx, "cron:wf:node", time.Minute)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ok {
+		t.Fatal("first lock was not acquired")
+	}
+	t.Cleanup(func() { _ = lock.Release(context.Background()) })
+
+	_, ok, err = primitives.TryLock(ctx, "cron:wf:node", time.Minute)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ok {
+		t.Fatal("second lock was acquired; want singleton behavior")
+	}
+}
